d68bb812f1fa7d7c49bf5fd88eda2c5d6326befb
mgitre <max.fitzkin@gmail.com>
2021-11-03 00:30:32 -0400

    Updated foxnews.py

--- scrapers/foxnews.py
+++ scrapers/foxnews.py
@@ -7,7 +7,7 @@ class FoxNews(BaseScraper):
         self.base_url = "https://www.foxnews.com"
         self.starting_pages = ["https://www.foxnews.com"]
         self.name = "foxnews"
-        self.url_exclusions = []
+        self.url_exclusions = ["cartoons-slideshow"]
         self.headline_matches = [("h1", {"class": "headline"})]
         self.subhead_matches = [("h2", {"class": "sub-headline"})]
         self.byline_matches = [("div", {"class": "author-byline"})]
